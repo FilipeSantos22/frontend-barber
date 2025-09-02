4d780b82968e3e8ae43085c6e43bfe81046755f9
FilipeSantos22 <filipeaprendiz@hotmail.com>
2025-09-02 14:29:52 -0300

    feat: Adiciona componentes Avatar e Badge, e integra no layout da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Header from './_components/header';
 import { SearchIcon } from "lucide-react";
 import { Button } from './_components/ui/button';
 import { Input } from './_components/ui/input';
+import { Badge } from './_components/ui/badge';
+import { Avatar, AvatarImage } from './_components/ui/avatar';
 import Image from "next/image";
+import { Card, CardContent } from './_components/ui/card';
 
 export default function Home() {
   return (
@@ -25,6 +28,31 @@ export default function Home() {
         <div className='relative w-full h-[150px] mt-6'>
             <Image src="/Banner01.png" alt="Agende conosco" fill className='object-cover rounded-xl' />
         </div>
+
+        <Card className='mt-6'>
+            <CardContent className='flex justify-between p-0'>
+                <div className='flex flex-col gap-2 py-5 pl-5'>
+                    
+                    <Badge className='w-fit'>Confirmado</Badge>
+                    <h3 className='semi-bold'>Corte de Cabelo</h3>
+
+                    <div className='flex items-center gap-2'>
+                        <Avatar className='w-6 h-6'>
+                            <AvatarImage src="/globe.svg"></AvatarImage>
+                        </Avatar>
+                    
+                        <p className='text-sm'>Barbearia FGS</p>
+                    </div>
+
+                </div>
+
+                <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+                    <p className='text-sm'>Setembro</p>
+                    <p className='text-2xl'>02</p>
+                    <p className='text-sm'>20:00</p>
+                </div>
+            </CardContent>
+        </Card>
       </div>
     </div>
 
